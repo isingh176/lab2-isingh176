0023db7bb5a35d604b8b8cb19b411b82a682d37c
Ishwinder Singh <isingh176@myseneca.ca>
2024-09-21 21:20:32 -0400

    Completed Lab2d

---
+++ lab2d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} name age")
+    sys.exit(0)  # Exit gracefully with code 0
+
+name = sys.argv[1]
+
+age = int(sys.argv[2])
+
+print(f"Hi {name}, you are {age} years old.")
+
