c18446bab2e51dd32600e7fb585c7543fb99266d
Ishwinder Singh <isingh176@myseneca.ca>
2024-09-21 22:09:33 -0400

    Completed Lab2a

--- lab2a.py
+++ lab2a.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
-#name = 'Jon'
-#age = 20
-#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
-
-colour = input("Type in a colour and press enter: ")
-print('The colour I typed in is: ' + colour)
-
+name = 'Jon'
+age = 20
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
 
