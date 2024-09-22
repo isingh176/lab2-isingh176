f27b56b4100ea98538a526d8307fc52f60ae4116
Ishwinder Singh <isingh176@myseneca.ca>
2024-09-21 21:26:44 -0400

    Completed Lab2e

---
+++ lab2e.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Author: Ishwinder Singh
+# Author ID: isingh176
+# Date Created: 2004/05/19
+
+# Initialize the timer object
+timer = 10
+
+# While loop to countdown from 10 to 1
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+# Print the final message
+print("blast off!")
+
